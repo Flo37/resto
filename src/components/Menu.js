bd0eb58cb88833d9c0017f6b058b45428d2d236a
Floris <munezerofloris@gmail.com>
2025-04-22 10:15:52 +0200

    edit

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,7 +5,7 @@ const Menu = ({meal}) => {
         <div>
             <li>{meal.strMeal}</li>
             <p>{meal.strCategory}</p>
-            <em>{meal.strArea}</em>
+            <em>From: {meal.strArea}</em>
             <img src={meal.strMealThumb} alt={meal.strMealThumb} />
             <a href={meal.strYoutube} ><h1>Tuto</h1></a>
             <h3>Instructions</h3>
